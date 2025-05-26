be9eed74ee1bf9db1e6fe4273a8a2fd006be13f7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-05-26 12:26:56 +0900

    feat: add config-server app

---
+++ infra/modules/config-server/app/build.gradle.kts
@@ -0,0 +1,58 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.codejsha"
+version = "0.1.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.1")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<BootJar> {
+    archiveVersion.set("")
+}
+
+tasks.withType<Jar> {
+    archiveVersion.set("")
+}
+
+tasks.processResources {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
