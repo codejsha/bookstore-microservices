8b05edc53ee8bb80ef7603a90ce3d7ac4f059cbb
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-06-06 02:31:32 +0900

    refactor: update cert-manager

--- infra/modules/cert-manager/main.tf
+++ infra/modules/cert-manager/main.tf
@@ -25,3 +25,16 @@ resource "helm_release" "cert_manager" {
     value = true
   }
 }
+
+resource "kubernetes_manifest" "cluster_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "selfsigned-cluster-issuer"
+    }
+    spec = {
+      selfSigned = {}
+    }
+  }
+}
