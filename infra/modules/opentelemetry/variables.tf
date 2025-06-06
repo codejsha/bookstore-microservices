b41cb9313b449f1d87543a06224767e5a0638592
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-06-07 02:17:46 +0900

    refactor: update opentelemetry

--- infra/modules/opentelemetry/variables.tf
+++ infra/modules/opentelemetry/variables.tf
@@ -2,3 +2,11 @@ variable "namespace" {
   description = "Namespace name"
   type        = string
 }
+
+variable "otel_collector_address" {
+  description = "OpenTelemetry Collector gRPC address"
+}
+
+variable "otel_collector_fqdn" {
+  description = "OpenTelemetry Collector gRPC FQDN"
+}
