9aa671e475f88c38d38412e476ad3bd3118552e9
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-06-08 03:20:33 +0900

    refactor: kubernetes auth

--- infra/modules/gitea/modules/ssh/main.tf
+++ infra/modules/gitea/modules/ssh/main.tf
@@ -16,4 +16,5 @@ resource "vault_kubernetes_auth_backend_role" "gitea_ssh_keys" {
   bound_service_account_names = ["gitea"]
   bound_service_account_namespaces = [var.namespace]
   token_policies = [vault_policy.gitea_ssh_keys.name]
+  token_ttl = "3600" # 1 hour
 }
