08534ec6a8605b9f529a21b17f9a9262c74d8c11
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-06-09 18:23:46 +0900

    refactor: update config-server

--- infra/modules/config-server/terraform/variables.tf
+++ infra/modules/config-server/terraform/variables.tf
@@ -2,3 +2,8 @@ variable "namespace" {
   description = "Namespace name"
   type        = string
 }
+
+variable "repository_ca_file" {
+  description = "Path to the CA file for the repository"
+  type        = string
+}
