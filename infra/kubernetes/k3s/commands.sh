c971fa99c3e605e86de90fff5613a9dd9a49e0e8
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-06-14 09:36:27 +0900

    refactor: remove reverse proxy

--- infra/kubernetes/k3s/commands.sh
+++ infra/kubernetes/k3s/commands.sh
@@ -2,3 +2,5 @@
 
 scp root@workstation.local:/var/lib/rancher/k3s/server/tls/server-ca.crt ../../modules/vault/ca.crt
 scp root@workstation.local:/var/lib/rancher/k3s/server/tls/server-ca.crt ~/.kube/k3s/server-ca.crt
+scp root@workstation.local:/var/lib/rancher/k3s/server/tls/client-admin.crt ~/.kube/k3s/client-admin.crt
+scp root@workstation.local:/var/lib/rancher/k3s/server/tls/client-admin.key ~/.kube/k3s/client-admin.key
